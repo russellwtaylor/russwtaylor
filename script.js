d50b501e525292443237360d092e9d1af8806af3
Russell Taylor <rssell.taylor@gmail.com>
2024-12-11 20:09:18 -0500

    matrix addition

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+function enterMatrix() {
+    console.log('Entering the Matrix...');
+
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log(canvas.width, canvas.height);
+
+    let gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop(0, '#008F11');
+    gradient.addColorStop(0.2, '#002B05');
+    gradient.addColorStop(0.4, '#000E02');
+    gradient.addColorStop(0.6, '#000E02');
+    gradient.addColorStop(0.8, '#002B05');
+    gradient.addColorStop(1, '#008F11');
+
+    class Symbol {
+        constructor(x, y, fontSize, canvasHeight) {
+            this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            this.x = x;
+            this.y = y;
+            this.fontSize = fontSize;
+            this.canvasHeight = canvasHeight;
+        }
+        draw(context) {
+            this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+            context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+            if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+                this.y = 0;
+            } else {
+                this.y += 1;
+            }
+        }
+    }
+
+    class Effect {
+        constructor(canvasWidth, canvasHeight) {
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
+            this.fontSize = 25;
+            this.columns = this.canvasWidth / this.fontSize;
+            this.symbols = [];
+            this.#init();
+        }
+        #init() {
+            for (let i = 0; i < this.columns; i++) {
+                this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+            }
+        }
+        resize(width, height) {
+            this.canvasWidth = width;
+            this.canvasHeight = height;
+            this.columns = this.canvasWidth / this.fontSize;
+            this.symbols = [];
+            this.#init();
+        }
+    }
+
+    const effect = new Effect(canvas.width, canvas.height);
+    let lastTime = 0;
+    const fps = 18;
+    const nextFrame = 1000 / fps;
+    let timer = 0;
+
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        if (timer > nextFrame) {
+            ctx.fillStyle = 'rgba(13,2,8,0.05)';
+            ctx.textAlign = 'center';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = gradient;//'#0aff0a';
+            ctx.font = effect.fontSize + 'px monospace';
+            effect.symbols.forEach(symbol => symbol.draw(ctx));
+            timer = 0;
+        } else {
+            timer += deltaTime;
+        }
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+
+    window.addEventListener('resize', function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        effect.resize(canvas.width, canvas.height);
+    });
+
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // Typed Text Effect
     const typedTextElement = document.querySelector('.typed-text');
