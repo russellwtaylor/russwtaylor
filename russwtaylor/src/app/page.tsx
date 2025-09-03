6fae2707c015c135ef167a3888894680a0955248
Russ Taylor <rssell.taylor@gmail.com>
2025-09-02 22:01:48 -0400

    Update front page layout

--- russwtaylor/src/app/page.tsx
+++ russwtaylor/src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
 			<h1 className="text-4xl font-bold text-center">
 				Russell Taylor&apos;s Portfolio Site
 			</h1>
-			<p className="text-lg text-center">Welcome to my portfolio!</p>
 
 			<div className="max-w-4xl text-center">
 				<p className="mb-4">
